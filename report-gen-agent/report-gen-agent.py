dc7072d1a9587b7465417c2c2ef305619028a66c
aihackdib-australia-team05-user0 <AIHackdib-australia-team05-user01@cloudlabs4copilot.onmicrosoft.com>
2025-09-11 03:53:20 +0000

    i made a slightly longer report

--- report-gen-agent/report-gen-agent.py
+++ report-gen-agent/report-gen-agent.py
@@ -14,7 +14,7 @@ model_client = AzureOpenAIChatCompletionClient(
     model="gpt-5-chat",
     api_version="2025-01-01-preview",
     azure_endpoint="https://aihac-mfc0kjaa-eastus2.cognitiveservices.azure.com/",
-    api_key="API",
+    api_key="API_KEY",
     model_info={"vision":False,"function_calling":True,"json_output":True,"family":"gpt-5","structured_output":True,"multiple_system_messages":True}
 )
 
